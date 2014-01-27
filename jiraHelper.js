c12cd3321779402c5c0328135f6372665e21d295
Daniel Blaszczyk <dblaszc@searshc.com>
2014-01-27 16:11:38 -0600

    eod commit

--- jiraHelper.js
+++ jiraHelper.js
@@ -7,25 +7,40 @@ shcLinkUpdate = {
 			var curURL = urlRegex.exec(allAnchor[x].getAttribute("href"));
 
 			if(curURL==="www.sears.com"||curURL==="www.kmart.com"||curURL==="staging.ch4.intra.sears.com"||curURL==="http://staging.ch4.intra.kmart.com/"){
-				updateLink(allAnchor[x]);
+				this.updateLink(allAnchor[x]);
 			}
 		}
 
 		console.log("SHC Helper Log: " + filtAnchor.length + " anchor tags updated!");
 	},
 	updateLink: function(upAnchor){
-		upAnchor.addEventListener("mouseover",linkPopup,false);
+		upAnchor.addEventListener("click",this.linkPopup,false);
 	},
 	linkPopup: function(evt){
 		evt.preventDefault();
 
 		var xCoord = evt.pageX,
-		yCoord = evt.pageY;
+		yCoord = evt.pageY,
+		popUp = document.createElement("div"),
+		liveLink = document.createElement("a"),
+		stageLink = document.createElement("a");
 
+		popUp.setAttribute("class", "shcHelperPU").setAttribute("style", "left: " + xCoord + "px; top: " + yCoord +"px;" );
+
+		if(curURL==="www.sears.com"||curURL==="staging.ch4.intra.sears.com"){
+			
+		}else{
+
+		}
+
+		liveLink.setAttribute("target", "_blank").setAttribute("href", "left: " + xCoord + "px; top: " + yCoord +"px;" );
+
+		stageLink.setAttribute("target", "_blank").setAttribute("href", "left: " + xCoord + "px; top: " + yCoord +"px;" );
+			
 		console.log("SHC Helper Log: xCoord = " + xCoord + " yCoord = " + yCoord);
 	}
 }
 
 document.addEventListener('DOMContentLoaded', function () {
- 	
+ 	shcLinkUpdate.getJiraLinks();
 });
